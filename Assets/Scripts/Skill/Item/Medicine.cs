52ae2e6083a05dbc9e7adae11cdb08d4dc4d33d0
Mike Murray <mikemurray056@gmail.com>
2022-09-26 12:17:24 -0400

    Added task to to do list.
    Medicine currently doesn't work outside of combat.

--- Assets/Scripts/Skill/Item/Medicine.cs
+++ Assets/Scripts/Skill/Item/Medicine.cs
@@ -6,22 +6,79 @@ public class Medicine : Skill
 {
     public override void Activate(Avatar user, Avatar target, Color borderColor)
     {
-        base.Activate(target, borderColor);
-        if (target.status == Avatar.Status.Poisoned || target.status == Avatar.Status.Paralyzed || 
-            target.status == Avatar.Status.Charmed || target.status == Avatar.Status.Blind)
+        //code execution depends on game state
+        /*GameManager gm = GameManager.instance;
+
+         if (target.status == Avatar.Status.Poisoned || target.status == Avatar.Status.Paralyzed || 
+                target.status == Avatar.Status.Charmed || target.status == Avatar.Status.Blind)
         {
             target.status = Avatar.Status.Normal;
-            ui.DisplayStatusUpdate("AILMENT REMOVED", target.transform.position);
+            if (gm.gameState == GameManager.GameState.Combat)
+            {
+                ui.DisplayStatusUpdate("CURED", target.transform.position);
+            }
+            else
+            {
+                DungeonUI ui = DungeonUI.instance;
+                ui.DisplayStatus("CURED", ui.partyDisplay[ui.currentHero].heroSprite.transform.position, ui.healColor);
+                ui.partyDisplay[ui.currentHero].UpdateUI();
+            }
         }
         else
         {
-            ui.DisplayStatusUpdate("NO EFFECT", target.transform.position);
+            if (gm.gameState == GameManager.GameState.Combat)
+            {
+                ui.DisplayStatusUpdate("NO EFFECT", target.transform.position);
+            }
+            else
+            {
+                DungeonUI ui = DungeonUI.instance;
+                ui.DisplayStatus("NO EFFECT", ui.partyDisplay[ui.currentHero].heroSprite.transform.position, ui.normalColor);
+                ui.partyDisplay[ui.currentHero].UpdateUI();
+            }
+        }*/
+        GameManager gm = GameManager.instance;
+        if (gm.gameState == GameManager.GameState.Combat)
+        {
+            base.Activate(target, borderColor);
+            if (target.status == Avatar.Status.Poisoned || target.status == Avatar.Status.Paralyzed || 
+                target.status == Avatar.Status.Charmed || target.status == Avatar.Status.Blind)
+            {
+                target.status = Avatar.Status.Normal;
+                ui.DisplayStatusUpdate("CURED", target.transform.position);
+            }
+            else
+            {
+                ui.DisplayStatusUpdate("NO EFFECT", target.transform.position);
+            }
+        }
+        else    //we're outside combat
+        {
+            DungeonUI ui = DungeonUI.instance;
+            if (target.status == Avatar.Status.Poisoned || target.status == Avatar.Status.Paralyzed || 
+                target.status == Avatar.Status.Charmed || target.status == Avatar.Status.Blind)
+            {
+                target.status = Avatar.Status.Normal;
+                ui.DisplayStatus("CURED", ui.partyDisplay[ui.currentHero].heroSprite.transform.position, ui.healColor);
+                ui.partyDisplay[ui.currentHero].UpdateUI();
+            }
+            else
+            {
+                ui.DisplayStatus("NO EFFECT", ui.partyDisplay[ui.currentHero].heroSprite.transform.position, ui.normalColor);
+            }
         }
     }
 
     //used outside of combat
-    public override void Activate(Avatar target)
+    /*public override void Activate(Avatar target)
     {
+        GameManager gm = GameManager.instance;
+        if (gm.gameState == GameManager.GameState.Normal)
+        {
+            DungeonUI ui = DungeonUI.instance;
+            ui.DisplayStatus(amount.ToString(), ui.partyDisplay[ui.currentHero].heroSprite.transform.position, ui.healColor);
+            ui.partyDisplay[ui.currentHero].UpdateUI();
+        }
         if (target.status == Avatar.Status.Poisoned || target.status == Avatar.Status.Paralyzed || 
             target.status == Avatar.Status.Charmed || target.status == Avatar.Status.Blind)
         {
@@ -32,5 +89,5 @@ public class Medicine : Skill
         {
             ui.DisplayStatusUpdate("NO EFFECT", target.transform.position);
         }
-    }
+    }*/
 }
